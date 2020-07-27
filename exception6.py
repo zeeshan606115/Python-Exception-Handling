7cf32a70de9a0f9bd8c8986cd566f452e133f028
Syed Zeeshan Husain <36918527+zeeshan606115@users.noreply.github.com>
2020-07-27 22:46:10 +0530

    Add files via upload

---
+++ exception6.py
@@ -0,0 +1,13 @@
+try:
+	x = int(input("Enter first number: "))
+	y = int(input("Enter second number: "))
+	print(x/y)
+
+except:
+	print("Except")
+
+else:
+	print("else")
+
+finally:
+	print("finally")
